0018f96f3f7d69d6c56c7eaacc192510e55dd66e
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-02 01:14:05 -0800

    Try out new approach

--- src/include/tinyusb_bindings.h
+++
@@ -1,3 +0,0 @@
-#if defined(ESP_IDF_COMP_ESPRESSIF__ESP_TINYUSB_ENABLED)
-#include "tinyusb.h"
-#endif
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use esp_idf_sys::esp_tinyusb::{tinyusb_config_t, tinyusb_driver_install};
-
+use esp_idf_sys::esp_tinyusb::{
+    tinyusb_config_t, tinyusb_driver_install, tinyusb_msc_storage_mount,
+};
+use std::ffi::CString;
 
 fn main() {
     let tusb_cfg = tinyusb_config_t::default();
-    unsafe { tinyusb_driver_install(&tusb_cfg); }
+    unsafe {
+        tinyusb_driver_install(&tusb_cfg);
+    }
+
+    let s = CString::new("");
+    unsafe { tinyusb_msc_storage_mount(s.unwrap().as_ptr()) };
 }
